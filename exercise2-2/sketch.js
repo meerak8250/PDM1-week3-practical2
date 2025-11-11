d53c08e15acbb55c3ad32decff94bfbc6a31a956
Meera Kofkin <dsx522@york.ac.uk>
2025-11-11 23:19:02 +0000

    Update sketch.js

--- exercise2-2/sketch.js
+++ exercise2-2/sketch.js
@@ -1,10 +1,11 @@
 function setup(){
     createCanvas(600, 600);
+    print(console.log)
 }
 
 function draw(){
 background(0);
-fill(250,202,160,100);
+fill(250,202,160, 100);
 circle(mouseX, mirrorCoordinate(mouseY), 50);
 fill(255, 255, 0, 100);
 circle(mouseX, mouseY, 50);
@@ -16,6 +17,7 @@ circle(mirrorCoordinate(mouseX), mouseY, 50);
 }    
 
 function mirrorCoordinate(coordinate){
-    const newCoordinate= width- coordinate;
-    return newCoordinate;
+    const NewCoordinate= width- coordinate;
+    return NewCoordinate;
 }
+
