67400a6930d594114f75b6f503ba8ea5a4be92a1
Meera Kofkin <dsx522@york.ac.uk>
2025-11-12 21:34:27 +0000

    Update sketch.js

--- exercise2-2/sketch.js
+++ exercise2-2/sketch.js
@@ -6,7 +6,7 @@ function setup(){
 function draw(){
 background(0);
 fill(250,202,160, 100);
-circle(300, mirrorCoordinate(500), 50);
+circle(mouseX, mirrorCoordinate(mouseY), 50);
 fill(255, 255, 0, 100);
 circle(mouseX, mouseY, 50);
 fill(255, 0, 255, 100);
